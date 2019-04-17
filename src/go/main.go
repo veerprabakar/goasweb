085a5348a153c19e2c6fb65e583e2e4eac1a3219
veerprabakar <prabakar.veer@gmail.com>
2019-04-17 21:21:15 +1200

    update 02

--- src/go/main.go
+++ src/go/main.go
@@ -12,33 +12,33 @@ func main() {
 	<-c
 }
 
-func add(i []js.Value) {
-	result := js.ValueOf(i[0].Int() + i[1].Int())
-	js.Global().Set("output", result)
-	println(result.String())
-}
-
-func sub(i []js.Value) {
-	result := js.ValueOf(i[0].Int() - i[1].Int())
-	js.Global().Set("output", result)
-	println(result.String())
-}
-
-func mul(i []js.Value) {
-	result := js.ValueOf(i[0].Int() * i[1].Int())
-	js.Global().Set("output", result)
-	println(result.String())
-}
-
-func div(i []js.Value) {
-	result := js.ValueOf(i[0].Int() / i[1].Int())
-	js.Global().Set("output", result)
-	println(result.String())
-}
-
 func registerCallbacks() {
 	js.Global().Set("add", js.NewCallback(add))
 	js.Global().Set("sub", js.NewCallback(sub))
 	js.Global().Set("mul", js.NewCallback(mul))
 	js.Global().Set("div", js.NewCallback(div))
+}
+
+func setResult(val js.Value) {
+	js.Global().Get("document").Call("getElementById", "output").Set("value", val)
+}
+
+func add(i []js.Value) {
+	result := js.ValueOf(i[0].Int() + i[1].Int())
+	setResult(result)
+}
+
+func sub(i []js.Value) {
+	result := js.ValueOf(i[0].Int() - i[1].Int())
+	setResult(result)
+}
+
+func mul(i []js.Value) {
+	result := js.ValueOf(i[0].Int() * i[1].Int())
+	setResult(result)
+}
+
+func div(i []js.Value) {
+	result := js.ValueOf(i[0].Int() / i[1].Int())
+	setResult(result)
 }
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,34 @@
 import '../styles/index.scss';
 
-console.log('webpack starterkit');
+const go = new Go();
+
+let mod, inst;
+
+WebAssembly.instantiateStreaming(fetch("./public/main.wasm"), go.importObject).then(
+    result => {
+        mod = result.module;
+        inst = result.instance;
+        go.run(inst);
+    }
+);
+
+// global calc function
+window.calc = function(op) {
+    let v1 = Number(document.getElementById("val1").value);
+    let v2 = Number(document.getElementById("val2").value);
+
+    if (!v1 || !v2) {
+        return;
+    }
+
+    // call the GO calc
+    if (op === '+') {
+        let result = add(v1, v2);
+    } else if (op === '-') {
+        let result = sub(v1, v2);
+    } else if (op === '*') {
+        let result = mul(v1, v2);
+    } else if (op === '/') {
+        let result = div(v1, v2);
+    }
+}
